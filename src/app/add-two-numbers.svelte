656c949224d50484b924117deecdb0f7345a1bd9
Chris <boyd111c@gmail.com>
2021-01-22 19:47:25 -0600

    improved formula

--- src/app/add-two-numbers.svelte
+++ src/app/add-two-numbers.svelte
@@ -1,7 +1,9 @@
 <script>
   let a = 1;
   let b = 3;
+  let c = 'cows'
 </script>
 <input type="number" bind:value={a}>
 <input type="number" bind:value={b}>
-<p>{a} + {b} = {a + b}</p>
+<input type="text" bind:value={c}>
+<p>{a} + {b} + {c} = {a + b + c}</p>
