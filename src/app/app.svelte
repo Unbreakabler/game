2c8595b1021d304762272c1f5f93b1632b2f1afa
Unbreakabler <jonboyd21@gmail.com>
2021-01-28 17:20:13 -0800

    run basic phaser canvas

--- src/app/app.svelte
+++ src/app/app.svelte
@@ -1,8 +1,11 @@
 
 <script>
   import AddTwoNumbers from './add-two-numbers.svelte'
+  import Game from './game.svelte'
 </script>
 
 <h1>esbuild svelte plugin example - test</h1>
 
 <AddTwoNumbers />
+
+<Game />
