b15b5135abefb3bfa488139f95e2408558bebfe1
Unbreakabler <jonboyd21@gmail.com>
2021-01-29 13:23:31 -0800

    fix ts

--- src/gamelogic/store.ts
+++ src/gamelogic/store.ts
@@ -3,6 +3,10 @@ import { writable } from "svelte/store";
 function createCount() {
   const { subscribe, set, update } = writable(0);
 
+  let test: any = 1
+
+  console.log('ts', test)
+
   return {
     subscribe,
     increment: () => update((n) => n + 1),
