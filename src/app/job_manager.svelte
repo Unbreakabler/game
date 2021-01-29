74cbf0768efa1c34c8d671619e57739ba6e4478a
Unbreakabler <jonboyd21@gmail.com>
2021-01-29 12:59:40 -0800

    change repo layout

---
+++ src/app/job_manager.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { count } from "../gamelogic/store.ts";
+  import ProgressBar from "../components/progress_bar.svelte";
+</script>
+
+<div>test</div>
+
+<ProgressBar current={$count} />
---
+++ src/components/progress_bar.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let total = 100;
+  export let current = 0;
+
+  $: progress_width = (current/total) * 100 + '%'
+</script>
+
+<style>
+  .container {
+    height: 20px;
+    width: 100%;
+    background-color: red;
+  }
+  .progress {
+    background-color: blue;
+    width: var(--progress_width);
+    height: 20px;
+  }
+</style>
+
+<div class="container" style="--progress_width: {progress_width}">
+  <div class="progress"></div>
+</div>
