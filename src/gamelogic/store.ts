3c6cd5cfb919fe3287a3406f786336640091a03b
Unbreakabler <jonboyd21@gmail.com>
2021-01-29 13:23:38 -0800

    move store

--- src/gamelogic/store.ts
+++ src/gamelogic/store.ts
@@ -3,10 +3,6 @@ import { writable } from "svelte/store";
 function createCount() {
   const { subscribe, set, update } = writable(0);
 
-  let test: any = 1
-
-  console.log('ts', test)
-
   return {
     subscribe,
     increment: () => update((n) => n + 1),
